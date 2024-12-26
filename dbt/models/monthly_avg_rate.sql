7d7163d246b8049ec20d2455dba7a99b990b203d
sdaaq <38110614+sdaaq@users.noreply.github.com>
2024-12-26 22:35:16 +0300

    Create monthly_avg_rate.sql

---
+++ dbt/models/monthly_avg_rate.sql
@@ -0,0 +1,31 @@
+{{
+  config(
+    materialized='incremental',
+    unique_key='month'
+  )
+}}
+
+WITH monthly_data AS (
+    SELECT
+        monthName(date) AS month,
+        'EUR' AS from_currency,
+        'USD' AS to_currency,
+        to_usd / to_eur AS rate
+    FROM
+        exchange_rates
+    {% if is_incremental() %}
+    WHERE
+        toStartOfMonth(date) > (SELECT MAX(toStartOfMonth(month)) FROM {{ this }})
+    {% endif %}
+)
+SELECT
+    month,
+    from_currency,
+    to_currency,
+    AVG(rate) AS rate
+FROM
+    monthly_data
+GROUP BY
+    month,
+    from_currency,
+    to_currency
